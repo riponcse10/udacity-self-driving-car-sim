0e8fa34a20578303c6d1a32c44eae2a05142700a
ripon <mahoque@uab.edu>
2021-11-08 20:09:35 -0600

    Initialize project

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/media/ripon/Windows4/Users/ahrip/Documents/linux-soft/ros-projects/udacity-sdv-ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/media/ripon/Windows4/Users/ahrip/Documents/linux-soft/ros-projects/catkin_ws/devel;/opt/ros/noetic'.split(';') if '/media/ripon/Windows4/Users/ahrip/Documents/linux-soft/ros-projects/catkin_ws/devel;/opt/ros/noetic' != '' else []
